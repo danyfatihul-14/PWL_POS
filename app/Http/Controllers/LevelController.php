4470f207f11c7e160f53c7d9bdd6ab1b7f415fad
Dany Fatihul Ihsan <danyarkedjo8@gmail.com>
2024-02-29 21:38:25 +0700

    ELOQUENT ORM

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)' , ['CUS','Pelanggan',now()]);
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)' , ['CUS','Customer',now()]);
         // return 'Insert data baru berhasil';
 
         // $row = DB::update('update m_level set level_nama =? where level_kode =?', ['Customer','CUS']);
