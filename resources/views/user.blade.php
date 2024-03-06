c98756c8ca9f5ec29a4b6d0edcc6c58d57420f3c
Dany Fatihul Ihsan <danyarkedjo8@gmail.com>
2024-03-06 10:39:19 +0700

    Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,16 +7,16 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspcing="0">
         <tr>
-            <td>Jumlah Pengguna</td>
-            {{-- <td>Username</td>
+            <td>ID</td>
+            <td>Username</td>
             <td>Nama</td>
-            <td>ID Level Pengguna</td> --}}
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
-            {{-- <td>{{ $data->username }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td> --}}
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
